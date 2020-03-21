419d2eb298796ef569394fa50dc090ad1cf0bb49
Kyle Umstatter <kumstatter@genvidtech.com>
2020-03-20 22:09:33 -0700

    update

--- streams.go
+++ streams.go
@@ -4,16 +4,20 @@ import "time"
 
 // Stream ...
 type Stream struct {
-	ID          int64         `json:"_id"`
-	Game        string        `json:"game"`
-	Viewers     int           `json:"viewers"`
-	VideoHeight int           `json:"video_height"`
-	AverageFps  int           `json:"average_fps"`
-	Delay       int           `json:"delay"`
-	CreatedAt   time.Time     `json:"created_at"`
-	IsPlaylist  bool          `json:"is_playlist"`
-	Preview     StreamPreview `json:"preview"`
-	Channel     StreamChannel `json:"channel"`
+	ID                int           `json:"_id"`
+	Game              string        `json:"game"`
+	BroadcastPlatform string        `json:"broadcast_platform"`
+	CommunityID       string        `json:"community_id"`
+	CommunityIds      []interface{} `json:"community_ids"`
+	Viewers           int           `json:"viewers"`
+	VideoHeight       int           `json:"video_height"`
+	AverageFps        int           `json:"average_fps"`
+	Delay             int           `json:"delay"`
+	CreatedAt         time.Time     `json:"created_at"`
+	IsPlaylist        bool          `json:"is_playlist"`
+	StreamType        string        `json:"stream_type"`
+	Preview           StreamPreview `json:"preview"`
+	Channel           StreamChannel `json:"channel"`
 }
 
 // StreamPreview ...
@@ -29,6 +33,7 @@ type StreamChannel struct {
 	Mature                       bool        `json:"mature"`
 	Status                       string      `json:"status"`
 	BroadcasterLanguage          string      `json:"broadcaster_language"`
+	BroadcasterSoftware          string      `json:"broadcaster_software"`
 	DisplayName                  string      `json:"display_name"`
 	Game                         string      `json:"game"`
 	Language                     string      `json:"language"`
@@ -38,12 +43,16 @@ type StreamChannel struct {
 	UpdatedAt                    time.Time   `json:"updated_at"`
 	Partner                      bool        `json:"partner"`
 	Logo                         string      `json:"logo"`
-	VideoBanner                  string      `json:"video_banner"`
-	ProfileBanner                string      `json:"profile_banner"`
-	ProfileBannerBackgroundColor interface{} `json:"profile_banner_background_color"`
+	VideoBanner                  interface{} `json:"video_banner"`
+	ProfileBanner                interface{} `json:"profile_banner"`
+	ProfileBannerBackgroundColor string      `json:"profile_banner_background_color"`
 	URL                          string      `json:"url"`
 	Views                        int         `json:"views"`
 	Followers                    int         `json:"followers"`
+	BroadcasterType              string      `json:"broadcaster_type"`
+	Description                  string      `json:"description"`
+	PrivateVideo                 bool        `json:"private_video"`
+	PrivacyOptionsEnabled        bool        `json:"privacy_options_enabled"`
 }
 
 // StreamResponse ...
